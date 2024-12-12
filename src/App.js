d9923b72ab79a46aa81ec2e111801f43fcf07454
bethinagaraju <148218951+bethinagaraju@users.noreply.github.com>
2024-12-12 19:07:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "./App.css"; // Ensure you have relevant CSS
 import Business from './Example'; 
-// import AudioPlayer from "./AudioPlayer";
-// import Timer from "./Timer";
 
 function App() {
   return (
     
       <>
-      {/* <Business />
-      <AudioPlayer /> */}
       <Business />
-      {/* < Timer /> */}
-      
       </>
-  
   );
 }
 
